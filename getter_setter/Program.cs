ce4f70b9a63877f2b54851af63bfd168564c04eb
Heiko Mathes <heiko.mathes@gmail.com>
2018-02-11 14:04:48 +0100

    :sparkles: add getter-setter exsamples

---
+++ getter_setter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace getter_setter
+{
+    class Program
+    {
+
+        // some public property of that class. Anyone can access/modify it
+        public string someProperty1 = "test";
+
+        // some custom implementation for a property
+        private string _someProperty2;
+        public string someProperty2
+        {
+            get
+            {
+                return this._someProperty2;
+            }
+
+            // getter and setter of the same property can have different accessors
+            private set
+            {
+                this._someProperty2 = value;
+            }
+        }
+
+        // an auto-implemented property. **This is equivalent to someProperty2**,
+        // but when the need arises to have a custom getter/setter for that property
+        // we can expand it with implementation
+        public string someProperty3 { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
